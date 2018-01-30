d7230da99ad78f4f0c8a0696f97b5a50d0d15ea1
Joe Cowton <joecowton@gmail.com>
2018-01-30 14:48:40 +0000

    adds fetch_post action creator

---
+++ src/components/posts_show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsShow extends Component {
+  render() {
+    return (
+      <div>
+        posts_show
+      </div>
+    )
+  }
+}
+
+export default PostsShow;
