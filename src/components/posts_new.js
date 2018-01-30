2576b2953273dc86df976f96762d4d2059afa87f
Joe Cowton <joecowton@gmail.com>
2018-01-30 12:38:38 +0000

    routing between index and post pages

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        post new
+      </div>
+
+    )
+  }
+}
+
+export default PostsNew;
