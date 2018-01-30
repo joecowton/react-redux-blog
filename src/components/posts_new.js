522cc2d87c8f34b88714ba750720bd9118089073
Joe Cowton <joecowton@gmail.com>
2018-01-30 12:52:41 +0000

    imports redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import { Field, redux-form } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return (
       <div>
-        post new
+        <form>
+          <Field
+            name="title"
+            component={}
+          />
+        </form>
       </div>
-
     )
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
